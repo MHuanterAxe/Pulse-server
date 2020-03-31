d75dffb9eff9a08065a69d6a3a7b8bca0e7fdd47
MHuanterAxe <maks.shkm@gmail.com>
2020-04-01 01:43:04 +0300

    fix: db connection

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   password: '45685255',
-  host: '0.0.0.0',
+  host: '84.38.180.139',
   port: 5432,
   database: 'PulseDB'
 })
