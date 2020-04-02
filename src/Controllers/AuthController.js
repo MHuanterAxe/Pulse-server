80ae66cd601901e441c90cc5d3c473289087b84e
MHuanterAxe <maks.shkm@gmail.com>
2020-04-02 23:12:46 +0300

    fix: auth controller

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -2,7 +2,6 @@ const { validationResult } = require( "express-validator");
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { secret } = require('../Config/default');
-const AuthHelper = require('../Helpers/AuthHelper')
 const { pool } = require('../DataBase');
 
 exports.register = async function (req, res) {
