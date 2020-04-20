8f4ac1a5111e8820c090b662a70e708a9a0dae68
MHuanterAxe <maks.shkm@gmail.com>
2020-04-20 17:04:47 +0300

    fix: adding default folder and tasklist, checking if default folder|tasklist exists

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -16,34 +16,43 @@ exports.register = async function (req, res) {
     }
 
     const { first_name, last_name, email, password } = req.body;
-    await pool.query('SELECT u.email FROM "Users" u WHERE u.email = $1', [email], async (err, result) => {
-      console.log(result)
-
-      if (result.rowCount) {
-        return res.status(400).json({ message: 'User with this email already exist!' })
-      }
-      const hashedPassword = bcrypt.hashSync(password, 12);
-      await pool.query(
-        'INSERT INTO "Users" (first_name, last_name, email, password) ' +
-        'VALUES ($1, $2, $3, $4)',
-        [ first_name, last_name, email, hashedPassword ]
-      ).catch(err => {
-        res.status(400).json('Incorrect data')
-      })
-      await pool.query(
-        'SELECT u.user_id FROM "Users" u WHERE u.email = $1', [email], async (err, result) => {
-          if (err) {
-            return res.status(400).json({ error: err })
-          }
-          await pool.query(
-            'INSERT INTO "Folders" (folder_label, user_id) ' +
-            'VALUES ($1, $2)',
-            [ "Все заметки", result.rows[0].user_id]
-          )
+    pool.query('SELECT u.email FROM "Users" u WHERE u.email = $1', [email])
+      .then(candidate => {
+        if (candidate.rowCount) {
+          return res.status(400).json({ message: 'User with this email already exist!' })
         }
-      )
-      return res.status(201).json({ message: 'Successfully signed up!' })
-    });
+        const hashedPassword = bcrypt.hashSync(password, 12);
+        pool.query(
+          'INSERT INTO "Users" (first_name, last_name, email, password) ' +
+          'VALUES ($1, $2, $3, $4)',
+          [ first_name, last_name, email, hashedPassword ])
+          .then(() => {
+            pool.query(
+              'SELECT u.user_id FROM "Users" u WHERE u.email = $1', [email])
+              .then(async result => {
+                console.log(result.rows)
+                await pool.query(
+                  'INSERT INTO "Folders" (user_id) VALUES ($1)',
+                  [result.rows[0].user_id]
+                ).catch(e => res.status(400).json({ error: 'err1' }));
+                await pool.query(
+                  'INSERT INTO "TaskLists" (user_id) VALUES ($1)',
+                  [result.rows[0].user_id]
+                ).catch(e => res.status(400).json({ error: err }));
+                return res.status(201).json({ message: 'Successfully signed up!' })
+              })
+              .catch(err => {
+                if (err) {
+                  return res.status(400).json({ error: 'err2' })
+                }
+              })
+          })
+          })
+          .catch(err => {
+            if (err) {
+              return res.status(400).json('Incorrect data')
+            }
+          })
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так' })
   }
@@ -58,19 +67,36 @@ exports.login = async function (req, res) {
       })
     }
     const {email, password} = req.body
-    await pool.query(
-      'SELECT * FROM "Users" u WHERE u.email = $1',
-      [email],
-      (err, result) => {
+    pool.query('SELECT * FROM "Users" u WHERE u.email = $1', [email])
+      .then(result => {
         if (result.rows.length === 0) {
           return res.status(400).json({ message: 'User did not found' })
         }
         const isMatch = bcrypt.compareSync(password, result.rows[0].password)
         if (isMatch) {
+          pool.query('SELECT t.user_id FROM "TaskLists" t WHERE t.task_list_label = \'Все задачи\' AND t.user_id = $1', [result.rows[0].user_id])
+            .then(defTaskList => {
+              if (defTaskList.rowCount === 0) {
+                pool.query('INSERT INTO "TaskLists" (user_id) VALUES ($1)', [result.rows[0].user_id])
+                  .catch(e => res.status(400).json({ error: err }));
+              }
+            });
+          pool.query('SELECT f.user_id FROM "Folders" f WHERE f.folder_label = \'Все заметки\' AND f.user_id = $1', [result.rows[0].user_id])
+            .then(defFolder => {
+              if (defFolder.rowCount === 0) {
+                pool.query('INSERT INTO "Folders" (user_id) VALUES ($1)', [result.rows[0].user_id])
+                  .catch(e => res.status(400).json({ error: err }));
+              }
+            })
           const token = jwt.sign({ id: result.rows[0].user_id }, secret)
           return res.json({ message: 'Successfully logging in', token })
         } else {
           return res.status(400).json({ message: 'Incorrect password' })
+        }
+      })
+      .catch(err => {
+        if (err) {
+          return res.status(400).json({ message: err })
         }
       })
 
